f6d7f1be39e0568c6dfca5515a252a37aad3c449
vlsk3104 <vlsk3104@gmail.com>
2022-03-20 20:17:56 +0900

    CSSでスタイリング

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,37 @@
 import React from "react";
+import "./style.css"
 
 export const App = () => {
   return(
     <>
-      <div>
+      <div className="input-area">
         <input type="" placeholder="TODOを入力" />
         <button>追加</button>
       </div>
-      <div>
-        <p>未完了のTODO</p>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
         <ul>
-          <div>
+          <div className="list-row">
             <li>テスト</li>
             <button>完了</button>
             <button>削除</button>
           </div>
-          <div>
+          <div className="list-row">
             <li>テスト2</li>
             <button>完了</button>
             <button>削除</button>
           </div>
         </ul>
-        <p>完了のTODO</p>
+      </div>
+      <div className="complete-area">
+        <p className="title">完了のTODO</p>
         <ul>
-          <div>
+          <div className="list-row">
             <li>テスト3</li>
             <button>戻す</button>
           </div>
         </ul>
-      </div>
+        </div>
       <div></div>
     </>
   );
