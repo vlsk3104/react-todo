0c1629ea089a1f5e22d7ec1e1b5f5c2276d2aef1
vlsk3104 <vlsk3104@gmail.com>
2022-03-21 10:20:46 +0900

    タスクの戻す機能

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ export const App = () => {
 
   const [todoText, setTodoText] = useState('');
 
-  const [incompleteTodos, setIncompleteTodos] = useState(['test1', 'test2']);
+  const [incompleteTodos, setIncompleteTodos] = useState([]);
 
-  const [completeTodos, setCompleteTodos] = useState(['test3']);
+  const [completeTodos, setCompleteTodos] = useState([]);
 
   const onChangeTodoText = (event) => setTodoText(event.target.value);
 
@@ -33,6 +33,15 @@ export const App = () => {
     setCompleteTodos(newCompleteTodos);
   };
 
+  const onClickBack = (index) => {
+    const newCompleteTodos = [...completeTodos];
+    newCompleteTodos.splice(index, 1);
+    const newIncompleteTodos = [...incompleteTodos, completeTodos[index]];
+
+    setCompleteTodos(newCompleteTodos);
+    setIncompleteTodos(newIncompleteTodos);
+  };
+
   return(
     <>
       <div className="input-area">
@@ -56,17 +65,16 @@ export const App = () => {
       <div className="complete-area">
         <p className="title">完了のTODO</p>
         <ul>
-        {completeTodos.map((todo) => {
+        {completeTodos.map((todo, index) => {
             return (
               <div key={todo} className="list-row">
                 <li>{todo}</li>
-                <button>戻す</button>
+                <button onClick={() => onClickBack(index)}>戻す</button>
               </div>
             );
           })}
         </ul>
-        </div>
-      <div></div>
+      </div>
     </>
   );
 };
