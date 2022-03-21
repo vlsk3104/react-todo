1588a3aeba329402e595726525fd9e3d165cfad7
vlsk3104 <vlsk3104@gmail.com>
2022-03-21 12:00:43 +0900

    カイゼン（TODOの上限設定）

--- src/App.jsx
+++ src/App.jsx
@@ -51,7 +51,12 @@ export const App = () => {
         todoText={todoText}
         onChange={onChangeTodoText}
         onClick={onClickAdd}
+        disabled={incompleteTodos.length >= 5}
       />
+      { incompleteTodos.length >= 5 && (
+        <p style={{ color: 'red' }}>登録できるtodo5個までだよ〜。消化しろ〜。</p>
+       )
+      }
       <IncompleteTodos
         todos={incompleteTodos}
         onClickComplete={onClickComplete}
